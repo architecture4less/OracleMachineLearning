9c1488069073ea44ced2c0abcf35536da0c60602
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-10-16 19:11:21 -0400

    main method pseudocode

--- src/me/jwotoole9141/oracleml/s4l1/GuessingGame.java
+++ src/me/jwotoole9141/oracleml/s4l1/GuessingGame.java
@@ -4,5 +4,58 @@ public class GuessingGame {
 
     public static void main(String[] args) {
 
+        // while true...
+
+            // clear screen
+
+            // ----------------------------------------------
+
+            // print a welcome message
+
+            // initialize list of user response choices
+
+            // if saved games are found...
+
+                // print that user should choose a game
+
+                // for each saved game...
+
+                    // print an option for that save
+
+                    // add the option to the list of choices
+
+            // else... print that no saves were found
+
+            // print a choice to create a new game
+
+            // ----------------------------------------------
+
+            // initialize user response
+
+            // while response is insufficient...
+
+                // get user response
+
+                // if response is valid... break loop
+
+            // ----------------------------------------------
+
+            // if response is to create a new game...
+
+                // do CREATE-NEW-GAME
+
+            // ----------------------------------------------
+
+            // if response is to play a game...
+
+                // do PLAY-GAME
+
+            // ----------------------------------------------
+
+            // if response is to quit...
+
+                // print goodbye message
+
+                // break main loop
     }
 }
