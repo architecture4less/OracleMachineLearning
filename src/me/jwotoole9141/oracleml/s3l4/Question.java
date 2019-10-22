dd33320d115d6092d9f86990c8632df77960319a
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-10-21 23:54:41 -0400

    begun to add linguistic structure to question class

--- src/me/jwotoole9141/oracleml/s3l4/Question.java
+++ src/me/jwotoole9141/oracleml/s3l4/Question.java
@@ -20,14 +20,82 @@ import java.util.Map;
 
 public class Question {
 
+    public static enum Relation {
+        IS_A("Is it a type of %s?",
+                "%s is a type of %s.",
+                new String[] {"type of", "kind of", "is a"}),
+
+        ISNT_A("Is it NOT a type of %s?",
+                "%s isn't a type of %s.",
+                new String[] {"type of", "kind of", "isnt a", "is not a"}),
+
+        IS("Is it %s?",
+                "%s is %s.",
+                new String[] {"is"}),
+
+        ISNT("Is it not %s?",
+                "%s isn't %s.",
+                new String[] {"isnt", "is not"}),
+
+        HAS("Does it have %s?",
+                "%s has %s.",
+                new String[] {"has"}),
+
+        HASNT("Does it NOT have %s?",
+                "%s hasn't %s",
+                new String[] {"hasnt", "has not", "doesnt have", "does not have"}),
+
+        DOES("Does it %s?",
+                "%s can %s.",
+                new String[] {"can", "can do"}),
+
+        DOESNT("Does it NOT %s?",
+                "%s doesn't %s",
+                new String[] {});
+
+        private String questionFmt;
+        private String confirmFmt;
+        private String[] declareKeys;
+
+        private Relation(String q, String c, String[] d) {
+            this.questionFmt = q;
+            this.confirmFmt = c;
+            this.declareKeys = d;
+        }
+
+        public String getQuestionFmt() {
+            return questionFmt;
+        }
+
+        public String getConfirmFmt() {
+            return confirmFmt;
+        }
+
+        public String[] getDeclareKeys() {
+            return declareKeys;
+        }
+    }
+
+    private @NotNull Relation relation;
+    private @NotNull String subject;
     private boolean last;
-    private @NotNull String prompt;
+
     private @Nullable Question yes;
     private @Nullable Question no;
 
-    public Question(boolean isLastQuestion, @NotNull String promptText) {
-        last = isLastQuestion;
-        prompt = promptText;
+    public Question(@NotNull Relation subjectRelation, @NotNull String subjectValue, boolean isLastQuestion) {
+
+        this.relation = subjectRelation;
+        this.subject = subjectValue;
+        this.last = isLastQuestion;
+    }
+
+    public Relation getRelation() {
+        return relation;
+    }
+
+    public String getSubject() {
+        return subject;
     }
 
     public boolean isLast() {
@@ -35,7 +103,7 @@ public class Question {
     }
 
     public @NotNull String getPrompt() {
-        return prompt;
+        return ;
     }
 
     public @Nullable Question getYes() {
