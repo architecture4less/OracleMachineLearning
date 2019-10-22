3f0bdc94dc4bef273fb09c8563a024e375270e3f
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-10-22 00:48:31 -0400

    subject relation interpreter

--- src/me/jwotoole9141/oracleml/s3l4/Question.java
+++ src/me/jwotoole9141/oracleml/s3l4/Question.java
@@ -15,64 +15,98 @@ package me.jwotoole9141.oracleml.s3l4;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Question {
 
-    public static enum Relation {
+    public enum Relation {
+
         IS_A("Is it a type of %s?",
                 "%s is a type of %s.",
-                new String[] {"type of", "kind of", "is a"}),
-
-        ISNT_A("Is it NOT a type of %s?",
                 "%s isn't a type of %s.",
-                new String[] {"type of", "kind of", "isnt a", "is not a"}),
+                new String[] { "is a type of", "is a kind of" },
+                new String[] { "isnt a type of", "is not a type of", "isnt a kind of", "is not a kind of" }),
 
         IS("Is it %s?",
                 "%s is %s.",
-                new String[] {"is"}),
-
-        ISNT("Is it not %s?",
                 "%s isn't %s.",
-                new String[] {"isnt", "is not"}),
+                new String[] { "is" },
+                new String[] { "isnt", "is not" }),
 
         HAS("Does it have %s?",
                 "%s has %s.",
-                new String[] {"has"}),
-
-        HASNT("Does it NOT have %s?",
-                "%s hasn't %s",
-                new String[] {"hasnt", "has not", "doesnt have", "does not have"}),
+                "%s hasn't %s.",
+                new String[] { "has" },
+                new String[] { "hasnt", "has not", "doesnt have", "does not have" }),
 
         DOES("Does it %s?",
                 "%s can %s.",
-                new String[] {"can", "can do"}),
+                "%s doesn't %s.",
+                new String[] { "can", "does" },
+                new String[] { "cant", "cannot", "can not", "doesnt", "does not" });
 
-        DOESNT("Does it NOT %s?",
-                "%s doesn't %s",
-                new String[] {});
+        private @NotNull String questionFormat;
+        private @NotNull String confirmFormat;
+        private @NotNull String negConfirmFormat;
+        private @NotNull String[] declareKeys;
+        private @NotNull String[] negDeclareKeys;
 
-        private String questionFmt;
-        private String confirmFmt;
-        private String[] declareKeys;
-
-        private Relation(String q, String c, String[] d) {
-            this.questionFmt = q;
-            this.confirmFmt = c;
+        Relation(@NotNull String q, @NotNull String c, @NotNull String nc, @NotNull String[] d, @NotNull String[] nd) {
+            this.questionFormat = q;
+            this.confirmFormat = c;
+            this.negConfirmFormat = nc;
             this.declareKeys = d;
+            this.negDeclareKeys = nd;
         }
 
-        public String getQuestionFmt() {
-            return questionFmt;
+        public @NotNull String getQuestionFormat() {
+            return questionFormat;
         }
 
-        public String getConfirmFmt() {
-            return confirmFmt;
+        public @NotNull String getConfirmFormat() {
+            return confirmFormat;
         }
 
-        public String[] getDeclareKeys() {
+        public @NotNull String getNegConfirmFormat() {
+            return negConfirmFormat;
+        }
+
+        public @NotNull String[] getDeclareKeys() {
             return declareKeys;
+        }
+
+        public @NotNull String[] getNegDeclareKeys() {
+            return negDeclareKeys;
+        }
+
+        public static Relation getRelation(String sentence) {
+
+            String sentenceLower = sentence.toLowerCase();
+
+            HashMap<Relation, List<String>> matches = new HashMap<>();
+            HashMap<Relation, List<String>> negMatches = new HashMap<>();
+
+            // for each subject relation type...
+            for (Relation rel : values()) {
+
+                // find any declare keys used...
+                matches.put(rel, new ArrayList<>());
+                for (String key : rel.declareKeys) {
+                    if (sentenceLower.contains(key)) {
+                        matches.get(rel).add(key);
+                    }
+                }
+                // find any neg declare keys used...
+                negMatches.put(rel, new ArrayList<>());
+                for (String key : rel.negDeclareKeys) {
+                    if (sentenceLower.contains(key)) {
+                        negMatches.get(rel).add(key);
+                    }
+                }
+            }
         }
     }
 
@@ -90,20 +124,20 @@ public class Question {
         this.last = isLastQuestion;
     }
 
-    public Relation getRelation() {
+    public @NotNull String getPrompt() {
+        return String.format(relation.getQuestionFormat(), subject);
+    }
+
+    public @NotNull Relation getRelation() {
         return relation;
     }
 
-    public String getSubject() {
+    public @NotNull String getSubject() {
         return subject;
     }
 
     public boolean isLast() {
         return last;
-    }
-
-    public @NotNull String getPrompt() {
-        return ;
     }
 
     public @Nullable Question getYes() {
@@ -124,18 +158,23 @@ public class Question {
 
     public @Nullable Map<String, Object> toMap() {
         Map<String, Object> map = new HashMap<>();
-        map.put("prompt", prompt);
+        map.put("relation", relation.name());
+        map.put("subject", subject);
         map.put("last", last);
         map.put("yes", yes);
         map.put("no", no);
         return map;
     }
 
-    public static @Nullable Question fromMap(@Nullable Map<?, ?> map) throws ClassCastException {
+    public static @Nullable Question fromMap(@Nullable Map<?, ?> map) throws IllegalArgumentException, ClassCastException {
         if (map == null) {
             return null;
         }
-        Question node = new Question((Boolean) map.get("last"), (String) map.get("prompt"));
+        Question node = new Question(
+                Relation.valueOf((String) map.get("relation")),
+                (String) map.get("subject"),
+                (Boolean) map.get("last")
+        );
         node.setYes(fromMap((Map<?, ?>) map.get("yes")));
         node.setNo(fromMap((Map<?, ?>) map.get("no")));
         return node;
