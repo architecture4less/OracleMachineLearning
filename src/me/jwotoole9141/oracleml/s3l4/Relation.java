ea2626fdd24e4d9e71c34cc569482cdca0f04218
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-10-28 01:23:26 -0400

    added answer enum added positivity attr added toPositivity() method

--- src/me/jwotoole9141/oracleml/s3l4/Relation.java
+++ src/me/jwotoole9141/oracleml/s3l4/Relation.java
@@ -12,7 +12,7 @@
 
 package me.jwotoole9141.oracleml.s3l4;
 
-import javafx.util.Pair;
+import org.javatuples.Pair;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -21,55 +21,72 @@ import java.util.regex.Pattern;
 
 public enum Relation {
 
-    OF("Is it a type of %s?",
-            "%s is a type of %s.",
-            Pattern.compile("(?<=is a (?:type|kind) of ).*",
-                    Pattern.CASE_INSENSITIVE)),
+    ANSWER("Is the answer '%s'?",
+            "%s, '%s', is the answer",
+            Pattern.compile("(?<=the answer is ).*",
+                    Pattern.CASE_INSENSITIVE),
+            true),
 
-    NOT_OF("Is it NOT a type of %s?",
-            "%s isn't a type of %s.",
+    OF("Is it a type of %s?",
+            "%s is a type of %s",
+            Pattern.compile("(?<=is a (?:type|kind) of ).*",
+                    Pattern.CASE_INSENSITIVE),
+            true),
+
+    NOT_OF("Is it a type of %s?",
+            "%s isn't a type of %s",
             Pattern.compile("(?<=is(?:n'?t| not) a (?:type|kind) of ).*",
-                    Pattern.CASE_INSENSITIVE)),
+                    Pattern.CASE_INSENSITIVE),
+            false),
 
     IS("Is it %s?",
-            "%s is %s.",
+            "%s is %s",
             Pattern.compile("(?<=is ).*",
-                    Pattern.CASE_INSENSITIVE)),
+                    Pattern.CASE_INSENSITIVE),
+            true),
 
-    IS_NOT("Is it NOT %s?",
-            "%s isn't %s.",
+    IS_NOT("Is it %s?",  // TODO negatives must come first so that 'not' doesn't get eaten up...
+            "%s isn't %s",
             Pattern.compile("(?<=is(?:n'?t| not) ).*",
-                    Pattern.CASE_INSENSITIVE)),
+                    Pattern.CASE_INSENSITIVE),
+            false),
 
     HAS("Does it have %s?",
-            "%s has %s.",
+            "%s has %s",
             Pattern.compile("(?<=(?:has|does have) ).*",
-                    Pattern.CASE_INSENSITIVE)),
+                    Pattern.CASE_INSENSITIVE),
+            true),
 
-    HAS_NOT("Does it NOT have %s?",
-            "%s hasn't %s.",
+    HAS_NOT("Does it have %s?",
+            "%s hasn't %s",
             Pattern.compile("(?<=(?:has(?:n'?t| not)|does(?:n'?t| not) have) ).*",
-                    Pattern.CASE_INSENSITIVE)),
+                    Pattern.CASE_INSENSITIVE),
+            false),
 
     DOES("Does it %s?",
-            "%s can %s.",
-            Pattern.compile("(?<=(?:can|does) ).*",
-                    Pattern.CASE_INSENSITIVE)),
+            "%s can %s",
+            Pattern.compile("(?<=can ).*",
+                    Pattern.CASE_INSENSITIVE),
+            true),
 
-    DOES_NOT("Does it NOT %s?",
-            "%s doesn't %s.",
-            Pattern.compile("(?<=(?:can(?:'?t| ?not))|does(?:n'?t| not) ).*",
-                    Pattern.CASE_INSENSITIVE));
+    DOES_NOT("Does it %s?",
+            "%s doesn't %s",
+            Pattern.compile("(?<=can(?:'?t| ?not) ).*",
+                    Pattern.CASE_INSENSITIVE),
+            false);
 
     private @NotNull String questionFormat;
     private @NotNull String statementFormat;
     private @NotNull Pattern statementPattern;
+    private boolean positive;
 
-    Relation(@NotNull String question, @NotNull String statement, @NotNull Pattern pattern) {
+    Relation(@NotNull String question, @NotNull String statement,
+            @NotNull Pattern pattern, boolean positive) {
 
         this.questionFormat = question;
         this.statementFormat = statement;
         this.statementPattern = pattern;
+        this.positive = positive;
     }
 
     public @NotNull String getQuestionFormat() {
@@ -82,6 +99,28 @@ public enum Relation {
 
     public @NotNull Pattern getStatementPattern() {
         return statementPattern;
+    }
+
+    public boolean isPositive() {
+        return positive;
+    }
+
+    public Relation toPositivity(boolean positivity) {
+
+        // answer is unsigned...
+        if (this != ANSWER) {
+            // odd enums are positive...
+            if ((ordinal() & 1) == 1) {
+                if (!positivity) {
+                    return values()[ordinal() + 1];
+                }
+            }
+            // even enums are negative...
+            else if (positivity) {
+                return values()[ordinal() - 1];
+            }
+        }
+        return this;
     }
 
     public static @Nullable Pair<Relation, String> parse(@NotNull String sentence) {
