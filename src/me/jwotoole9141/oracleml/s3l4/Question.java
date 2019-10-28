e72b53a458b71990c44de17d0918cf3591449cfe
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-10-28 01:21:30 -0400

    added overloaded getStatement()

--- src/me/jwotoole9141/oracleml/s3l4/Question.java
+++ src/me/jwotoole9141/oracleml/s3l4/Question.java
@@ -21,20 +21,26 @@ public class Question {
 
     private @NotNull Relation relation;
     private @NotNull String subject;
-    private boolean last;
 
     private @Nullable Question yes;
     private @Nullable Question no;
 
-    public Question(@NotNull Relation subjectRelation, @NotNull String subjectValue, boolean isLastQuestion) {
+    public Question(@NotNull Relation subjectRelation, @NotNull String subjectValue) {
 
         this.relation = subjectRelation;
         this.subject = subjectValue;
-        this.last = isLastQuestion;
     }
 
     public @NotNull String getPrompt() {
         return String.format(relation.getQuestionFormat(), subject);
+    }
+
+    public @NotNull String getStatement(@NotNull String answer) {
+        return String.format(relation.getStatementFormat(), answer, subject);
+    }
+
+    public @NotNull String getStatement(@NotNull String answer, boolean positivity) {
+        return String.format(relation.toPositivity(positivity).getStatementFormat(), answer, subject);
     }
 
     public @NotNull Relation getRelation() {
@@ -46,23 +52,32 @@ public class Question {
     }
 
     public boolean isLast() {
-        return last;
+        return relation == Relation.ANSWER;
     }
 
     public @Nullable Question getYes() {
-        return last ? null : yes;
+        return isLast() ? null : yes;
     }
 
     public @Nullable Question getNo() {
-        return last ? null : no;
+        return isLast() ? null : no;
     }
 
     public void setYes(@Nullable Question yesAnswer) {
-        this.yes = last ? null : yesAnswer;
+        this.yes = isLast() ? null : yesAnswer;
     }
 
     public void setNo(@Nullable Question noAnswer) {
-        this.no = last ? null : noAnswer;
+        this.no = isLast() ? null : noAnswer;
+    }
+
+    public void setAnswer(@NotNull Boolean answer, @Nullable Question nextQuestion) {
+        if (answer) {
+            setYes(nextQuestion);
+        }
+        else {
+            setNo(nextQuestion);
+        }
     }
 
     public @Nullable Map<String, Object> toMap() {
@@ -70,9 +85,8 @@ public class Question {
         Map<String, Object> map = new HashMap<>();
         map.put("relation", getRelation().name());
         map.put("subject", getSubject());
-        map.put("last", isLast());
         map.put("yes", (getYes() == null) ? null : getYes().toMap());
-        map.put("no", (getNo() == null) ? null : getNo().toMap());
+        map.put("zno", (getNo() == null) ? null : getNo().toMap());
         return map;
     }
 
@@ -84,17 +98,16 @@ public class Question {
         }
         Question node = new Question(
                 Relation.valueOf((String) map.get("relation")),
-                (String) map.get("subject"),
-                (Boolean) map.get("last")
+                (String) map.get("subject")
         );
         node.setYes(fromMap((Map<?, ?>) map.get("yes")));
-        node.setNo(fromMap((Map<?, ?>) map.get("no")));
+        node.setNo(fromMap((Map<?, ?>) map.get("zno")));
         return node;
     }
 
     @Override
     public String toString() {
         return String.format("Question[relation=%s, subject='%s', last=%b, yes=%s, no=%s]",
-                relation.name(), subject, last, yes == null ? "null" : "...", no == null ? "null" : "...");
+                relation.name(), subject, isLast(), yes == null ? "null" : "...", no == null ? "null" : "...");
     }
 }
