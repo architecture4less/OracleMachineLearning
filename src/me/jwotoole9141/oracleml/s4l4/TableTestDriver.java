337eafed454b31ce3b7e22af71932c7f4c64f393
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-02 00:41:25 -0500

    added more test drivers

---
+++ src/me/jwotoole9141/oracleml/s4l4/TableTestDriver.java
@@ -0,0 +1,20 @@
+/*
+ * AUTH: Jared O'Toole
+ * DATE: 12/2/2019 12:40 AM
+ * PROJ: OracleMachineLearning
+ * FILE: TableTestDriver.java
+ *
+ * "Create an ID3 Tree" task for Section 4 Lesson 4
+ * of the AI with ML in Java Oracle iLearning Course.
+ *
+ * Defines the TableTestDriver driver class.
+ */
+
+package me.jwotoole9141.oracleml.s4l4;
+
+public class TableTestDriver {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
