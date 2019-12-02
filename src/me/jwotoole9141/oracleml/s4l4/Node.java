7387d45c074ad845b3752534b1448cdc763c99b6
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-01 23:32:24 -0500

    added javadoc

--- src/me/jwotoole9141/oracleml/s4l4/Node.java
+++ src/me/jwotoole9141/oracleml/s4l4/Node.java
@@ -21,13 +21,33 @@ import org.jetbrains.annotations.Nullable;
 import java.lang.ref.WeakReference;
 import java.util.Map;
 
+/**
+ * Represents a node in a non-binary tree.
+ *
+ * @param <Q> The 'question' type for the node's label
+ * @param <A> The 'answer' type for the map-keys of the node's children
+ * @author Jared O'Toole
+ */
 public class Node<Q, A> {
+
+    /*
+     * FIXME node should really be broken into
+     *  two different classes extending a base node.
+     *  - AbsNode { parent, key }
+     *  - Node extends AbsNode { label, children }
+     *  - LeafNode extends AbsNode { result }
+     */
 
     private @NotNull Q label;
     private @NotNull WeakReference<Node> parent;
     private @NotNull ObservableMap<@NotNull A, @NotNull Node> children;
     private @Nullable Boolean result;
 
+    /**
+     * Creates a new node with the given label.
+     *
+     * @param label the node's 'question'
+     */
     public Node(@NotNull Q label) {
         this(label, null, null, null);
     }
@@ -62,18 +82,48 @@ public class Node<Q, A> {
         }
     }
 
+    /**
+     * Gets an object that represents the premise for
+     * how to choose an 'answer' from this node.
+     *
+     * @return the node's 'question'
+     */
     public @NotNull Q getLabel() {
         return label;
     }
 
+    /**
+     * Gets the parent of this node if it exists.
+     * Its reference is held weakly.
+     *
+     * @return the parent node or null
+     */
     public @Nullable Node getParent() {
         return parent.get();
     }
 
+    /**
+     * Gets the children of this node. Child nodes are
+     * accessed by a mapped key that represents the answer
+     * to this node's 'question'.
+     *
+     * @return a mapping of 'answers' to sub nodes
+     */
     public @NotNull Map<@NotNull A, @NotNull Node> getChildren() {
         return children;
     }
 
+    /**
+     * Gets the success or failure of this leaf node. This
+     * represents the answer to the premise of the entire tree.
+     * <br />
+     * <br />
+     * This method should <b><i>only</i></b> be called on a leaf
+     * node. Use {@link #isLeaf()} to check before calling.
+     *
+     * @return true if this leaf node represents a success, else false
+     * @throws IllegalStateException called on a non-leaf node
+     */
     public boolean getResult() {
         if (!isLeaf()) {
             throw new IllegalStateException(
@@ -84,6 +134,12 @@ public class Node<Q, A> {
         return result;
     }
 
+    /**
+     * Tests if this is a leaf node or not. A leaf node has
+     * a result but does not have a label or children.
+     *
+     * @return true if this is a leaf node, else false
+     */
     public final boolean isLeaf() {
 
         // enforce nodes having either a result or children...
@@ -94,6 +150,12 @@ public class Node<Q, A> {
         return result != null;
     }
 
+    /**
+     * Sets the result of this leaf node to be
+     * either success or failure.
+     *
+     * @param result true if success, else false
+     */
     public void setResult(boolean result) {
         this.result = result;
     }
