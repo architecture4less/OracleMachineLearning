ac929d570d20f2c9d7151331954901bea5812c3b
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-03 00:57:53 -0500

    using opencsv lib

--- src/me/jwotoole9141/oracleml/s4l4/DataTable.java
+++ src/me/jwotoole9141/oracleml/s4l4/DataTable.java
@@ -12,6 +12,13 @@
 
 package me.jwotoole9141.oracleml.s4l4;
 
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+import com.opencsv.exceptions.CsvValidationException;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -21,20 +28,26 @@ public class DataTable {
 
     public static class Column<T> {
 
+        private final String label;
         private final List<T> rows = new ArrayList<>();
         private final Set<T> values = new HashSet<>();
-        private List<T> rowsView = Collections.unmodifiableList(rows);
-        private Set<T> valuesView = Collections.unmodifiableSet(values);
+        private final List<T> rowsView = Collections.unmodifiableList(rows);
+        private final Set<T> valuesView = Collections.unmodifiableSet(values);
 
-        public Column() {
-            this(null);
+        public Column(String label) {
+            this(label, null);
         }
 
-        public Column(List<T> rows) {
+        public Column(String label, List<T> rows) {
+            this.label = label;
             if (rows != null) {
                 this.rows.addAll(rows);
                 this.values.addAll(rows);
             }
+        }
+
+        public String getLabel() {
+            return label;
         }
 
         public List<T> getRows() {
@@ -63,45 +76,51 @@ public class DataTable {
             return distr;
         }
 
-        public List<Integer> subColumnIndices(Predicate<T> filter) {
+        public Column<T> toSubColumn(Predicate<T> filter) {
+            return new Column<>(label, IntStream.range(0, rows.size())
+                    .filter(i -> filter.test(rows.get(i)))
+                    .boxed()
+                    .map(rows::get)
+                    .collect(Collectors.toList()));
+        }
+
+        public Column<T> toSubColumn(List<Integer> indices) {
+            return new Column<>(label, indices.stream()
+                    .map(rows::get)
+                    .collect(Collectors.toList()));
+        }
+
+        public List<Integer> toSubColumnIndices(Predicate<T> filter) {
             return IntStream.range(0, rows.size())
                     .filter(i -> filter.test(rows.get(i)))
                     .boxed()
                     .collect(Collectors.toList());
         }
-
-        public Column<T> toSubColumn(List<Integer> indices) {
-            return new Column<>(indices.stream()
-                    .map(rows::get)
-                    .collect(Collectors.toList()));
-        }
-
-        public Column<T> toSubColumn(Predicate<T> filter) {
-            return new Column<>(IntStream.range(0, rows.size())
-                    .filter(i -> filter.test(rows.get(i)))
-                    .boxed()
-                    .map(rows::get)
-                    .collect(Collectors.toList()));
-        }
     }
+
+    private final String title;
+    private final int numRows;
+    private final int size;
 
     private final List<Column> cols = new ArrayList<>();
     private final List<Column> colsView = Collections.unmodifiableList(cols);
 
-    private int numRows = 0;
-    private int size = 0;
-
-    public DataTable() {
-        this(null);
+    public DataTable(String title) {
+        this(title, null);
     }
 
-    public DataTable(Set<Column> columns) {
+    public DataTable(String title, Set<Column> columns) {
+
+        this.title = title;
 
         if (columns != null) {
             this.cols.addAll(columns);
         }
-        if (!this.cols.isEmpty()) {
-
+        if (this.cols.isEmpty()) {
+            this.numRows = 0;
+            this.size = 0;
+        }
+        else {
             this.numRows = this.cols.get(0).rows.size();
             this.size = numRows * this.cols.size();
 
@@ -111,6 +130,10 @@ public class DataTable {
                 }
             }
         }
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public List<Column> getColumns() {
@@ -126,13 +149,13 @@ public class DataTable {
     }
 
     public DataTable toSubTable(Predicate<Column> columnFilter) {
-        return new DataTable(cols.stream().filter(columnFilter).collect(Collectors.toSet()));
+        return new DataTable(title, cols.stream().filter(columnFilter).collect(Collectors.toSet()));
     }
 
     public <T> DataTable toSubTable(Column<T> filterColumn, Predicate<T> rowFilter) {
         if (cols.contains(filterColumn)) {
-            List<Integer> indices = filterColumn.subColumnIndices(rowFilter);
-            return new DataTable(cols.stream().map(c -> c.toSubColumn(indices)).collect(Collectors.toSet()));
+            List<Integer> indices = filterColumn.toSubColumnIndices(rowFilter);
+            return new DataTable(title, cols.stream().map(c -> c.toSubColumn(indices)).collect(Collectors.toSet()));
         }
         return this;
     }
@@ -147,11 +170,26 @@ public class DataTable {
 
     public String toCSV() {
 
+        StringWriter strWriter = new StringWriter();
+        CSVWriter csvWriter = new CSVWriter(strWriter);
+
         return null;  // TODO
     }
 
     public static DataTable fromCSV(String csv) {
 
+        try {
+            StringReader strReader = new StringReader(csv);
+            CSVReader csvReader = new CSVReader(strReader);
+
+            String[] line;
+            while ((line = csvReader.readNext()) != null) {
+
+            }
+        }
+        catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
         return null;  // TODO
     }
 }
