46f25b2dd1e1cb5bc785fd427636b3762d069779
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-04 04:00:58 -0500

    entropy and gain tests successful

--- src/me/jwotoole9141/oracleml/s4l4/driver/ID3TestDriver.java
+++ src/me/jwotoole9141/oracleml/s4l4/driver/ID3TestDriver.java
@@ -17,37 +17,58 @@ import me.jwotoole9141.oracleml.s4l4.Tree;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class ID3TestDriver {
 
     public static void main(String[] args) {
 
+        // create sample data...
+
+        Set<Integer> studyTrueCriteria = IntStream.range(1, 24).boxed().collect(Collectors.toSet());
+        DataTable.Column<Integer> studyHoursCol = new DataTable.Column<>("hours studied",
+                Arrays.asList(0, 0, 0, 0));
+
+        Set<CoinFlip> coinTrueCriteria = Collections.singleton(CoinFlip.HEADS);
+        DataTable.Column<CoinFlip> coinFlipCol = new DataTable.Column<>("coin flip",
+                Arrays.asList(CoinFlip.HEADS, CoinFlip.TAILS, CoinFlip.TAILS, CoinFlip.TAILS));
+
+        Set<Character> testTrueCriteria = new HashSet<>(Arrays.asList('A', 'B', 'C'));
+        DataTable.Column<Character> testGradeCol = new DataTable.Column<>("test grade",
+                Arrays.asList('C', 'B', 'F', 'D'));
+
+        DataTable table = new DataTable("Study Hours vs Coin Toss vs Test Grade",
+                Arrays.asList(studyHoursCol, coinFlipCol, testGradeCol));
+
+        System.out.println(table.toDiagram());
+
         // testing the entropy function...
 
-        {
-            Set<Boolean> successCriteria = Collections.singleton(true);
+        System.out.println("Testing Tree.Aglorithm.entropy()...");
 
-            DataTable.Column<Boolean> results = new DataTable.Column<>("test passed",
-                    Arrays.asList(true, false, true, true, false, false, true, false, false, false));
+        double studyHoursEntropy = Tree.Algorithm.entropy(studyHoursCol, studyTrueCriteria);
+        System.out.printf("The entropy of '%s' is: %f\n", studyHoursCol.getLabel(), studyHoursEntropy);
 
-            double entropy = Tree.Algorithm.entropy(results, successCriteria);
+        double coinFlipEntropy = Tree.Algorithm.entropy(coinFlipCol, coinTrueCriteria);
+        System.out.printf("The entropy of '%s' is: %f\n", coinFlipCol.getLabel(), coinFlipEntropy);
 
-            System.out.println("\nTesting Tree.Aglorithm.entropy()...");
-            System.out.println(results.toDiagram());
-            System.out.println("The entropy of the above column is: " + entropy);
-        }
-        {
-            Set<CoinFlip> successCriteria = Collections.singleton(CoinFlip.HEADS);
-            DataTable.Column<CoinFlip> results = new DataTable.Column<>("coin flip",
-                    Arrays.asList(CoinFlip.HEADS, CoinFlip.TAILS, CoinFlip.TAILS, CoinFlip.TAILS));
+        double testGradeEntropy = Tree.Algorithm.entropy(testGradeCol, testTrueCriteria);
+        System.out.printf("The entropy of '%s' is: %f\n", testGradeCol.getLabel(), testGradeEntropy);
 
-            double entropy = Tree.Algorithm.entropy(results, successCriteria);
+        // testing the gain function...
 
-            System.out.println("\nTesting Tree.Aglorithm.entropy()...");
-            System.out.println(results.toDiagram());
-            System.out.println("The entropy of the above column is: " + entropy);
-        }
+        System.out.println("\nTesting Tree.Algorithm.gain()...");
+
+        double hoursGainOnGrade = Tree.Algorithm.gain(testGradeEntropy, studyHoursCol, testGradeCol, testTrueCriteria);
+        System.out.printf("The gain of '%s' on '%s' is: %f\n",
+                studyHoursCol.getLabel(), testGradeCol.getLabel(), hoursGainOnGrade);
+
+        double flipGainOnGrade = Tree.Algorithm.gain(testGradeEntropy, coinFlipCol, testGradeCol, testTrueCriteria);
+        System.out.printf("The gain of '%s' on '%s' is: %f\n",
+                coinFlipCol.getLabel(), testGradeCol.getLabel(), flipGainOnGrade);
     }
 
     public enum CoinFlip {HEADS, TAILS}
