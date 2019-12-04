00ccc42f7d3aa7d029c1186bb9ab6722f0cb8a98
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-03 22:49:33 -0500

    created tree class

---
+++ src/me/jwotoole9141/oracleml/s4l4/Tree.java
@@ -0,0 +1,23 @@
+/*
+ * AUTH: Jared O'Toole
+ * DATE: 12/3/2019 10:46 PM
+ * PROJ: OracleMachineLearning
+ * FILE: Tree.java
+ *
+ * "Create an ID3 Tree" task for Section 4 Lesson 4
+ * of the AI with ML in Java Oracle iLearning Course.
+ *
+ * Defines the Tree class.
+ */
+
+package me.jwotoole9141.oracleml.s4l4;
+
+public class Tree {
+
+    public enum Algorithm { ID3 }
+
+    public static Node fromTable(DataTable table, Algorithm algorithm) {
+
+        return null;  // TODO
+    }
+}
