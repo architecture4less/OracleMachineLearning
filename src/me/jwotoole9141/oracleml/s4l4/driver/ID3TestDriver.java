c9fe1d4de92d7115dddcd7d065e5bdc2f12bdbed
Jared O'Toole <jwotoole9141@eagle.fgcu.edu>
2019-12-04 03:13:30 -0500

    added entropy function tests

--- src/me/jwotoole9141/oracleml/s4l4/driver/ID3TestDriver.java
+++ src/me/jwotoole9141/oracleml/s4l4/driver/ID3TestDriver.java
@@ -12,9 +12,43 @@
 
 package me.jwotoole9141.oracleml.s4l4.driver;
 
+import me.jwotoole9141.oracleml.s4l4.DataTable;
+import me.jwotoole9141.oracleml.s4l4.Tree;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Set;
+
 public class ID3TestDriver {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        // testing the entropy function...
+
+        {
+            Set<Boolean> successCriteria = Collections.singleton(true);
+
+            DataTable.Column<Boolean> results = new DataTable.Column<>("test passed",
+                    Arrays.asList(true, false, true, true, false, false, true, false, false, false));
+
+            double entropy = Tree.Algorithm.entropy(results, successCriteria);
+
+            System.out.println("\nTesting Tree.Aglorithm.entropy()...");
+            System.out.println(results.toDiagram());
+            System.out.println("The entropy of the above column is: " + entropy);
+        }
+        {
+            Set<CoinFlip> successCriteria = Collections.singleton(CoinFlip.HEADS);
+            DataTable.Column<CoinFlip> results = new DataTable.Column<>("coin flip",
+                    Arrays.asList(CoinFlip.HEADS, CoinFlip.TAILS, CoinFlip.TAILS, CoinFlip.TAILS));
+
+            double entropy = Tree.Algorithm.entropy(results, successCriteria);
+
+            System.out.println("\nTesting Tree.Aglorithm.entropy()...");
+            System.out.println(results.toDiagram());
+            System.out.println("The entropy of the above column is: " + entropy);
+        }
     }
+
+    public enum CoinFlip {HEADS, TAILS}
 }
